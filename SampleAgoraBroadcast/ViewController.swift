77b29631983304ed5e89eb2a04257b62fc715218
Roger Carvalho <rogerdcarvalho@users.noreply.github.com>
2020-08-03 08:05:08 +0100

    Initial Commit

---
+++ SampleAgoraBroadcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleAgoraBroadcast
+//
+//  Created by Roger Carvalho on 03/08/2020.
+//  Copyright © 2020 Roger Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleAgoraBroadcastTests/SampleAgoraBroadcastTests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleAgoraBroadcastTests.swift
+//  SampleAgoraBroadcastTests
+//
+//  Created by Roger Carvalho on 03/08/2020.
+//  Copyright © 2020 Roger Carvalho. All rights reserved.
+//
+
+import XCTest
+@testable import SampleAgoraBroadcast
+
+class SampleAgoraBroadcastTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
