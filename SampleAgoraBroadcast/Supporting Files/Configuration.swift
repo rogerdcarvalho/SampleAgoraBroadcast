47dcb51d938db0a0701f72f48aecbd2948fbd471
Roger Carvalho <rogerdcarvalho@users.noreply.github.com>
2020-08-07 22:09:35 +0100

    Update temp token

--- SampleAgoraBroadcast/Supporting Files/Configuration.swift
+++ SampleAgoraBroadcast/Supporting Files/Configuration.swift
@@ -12,7 +12,7 @@ import Foundation
 struct Configuration {
     static let AppId = "96fd8441c20247d5b5c8cafa079ca29b"
     static let ChannelName = "rdcMedia"
-    static let Token = "0067fb680be3b8242b7a7be3dd220e4343aIACl5CyZcl1VZRUVfYJ74UyCtho3N6FNw9+VdMCg+RNaq5MlARcAAAAAEABS+F3VdqstXwEAAQB1qy1f"
+    static let Token = "0067fb680be3b8242b7a7be3dd220e4343aIADJkxL7RHwd8aIuGRIQT3slkI54sKwZALsGZ8EXSQo/a5MlARcAAAAAEAA5dvKzEhQvXwEAAQARFC9f"
     static var StreamId = 10451
     static let DefaultSurveyMonkeyCode = "7FDV9CZ"
 
